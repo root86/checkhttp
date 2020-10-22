c93e253cfbb68cc42b83640803f0d80f3bdf28bd
root86 <manage0791@vip.qq.com>
2020-10-22 11:00:59 +0800

    Update checkhttp.sh

--- checkhttp.sh
+++ checkhttp.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-url="http://120.25.164.233:8080/appstore/app/checkversion?id=21"
 function get_json_value()
 {
   local json=$1
   local key=$2
-
+  
   if [[ -z "$3" ]]; then
     local num=1
   else
@@ -13,8 +12,7 @@ function get_json_value()
   fi
 
   local value=$(echo "${json}" | awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'${key}'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p)
-
   echo ${value}
 }
 
-get_json_value $(curl -s $url) versionCode
+get_json_value $(curl -s http://120.25.164.233:8080/appstore/app/checkversion?id=21) version
